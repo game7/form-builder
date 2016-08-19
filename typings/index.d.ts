a324c375be1ae8e93c95b4daf9555dfb7734e855
Chris Woodall <cmwoodall@game7.net>
2016-08-18 20:26:25 -0700

    add es6-promise d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react-router/history/index.d.ts" />
 /// <reference path="globals/react-router/index.d.ts" />
