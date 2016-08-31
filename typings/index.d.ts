60966c88ac03b8c57669e03ce8d0d335b8e0614a
Chris Woodall <cmwoodall@game7.net>
2016-08-31 15:51:47 -0700

    es5 support
    to enable compatibility with IE11

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react-router/history/index.d.ts" />
